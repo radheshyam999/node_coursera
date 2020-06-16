9891d7e37176fc0b0b178888d99210ff222815e3
radheshyam999 <radheshyamdubey999@gmail.com>
2020-06-16 21:55:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    //write("hey ladies and gentelman  where are you from?")
+    
+    res.send('Hello World! </br> How are you doing? </br>  Where are belong from?')
+});
+
+app.listen(8000, () => {
+    console.log('Example app listening on port 8000!')
+    console.log('Hello this is console log!')
+});
